0ec3752d385bd10ed0abefe5c2d824afbe316fed
Maiara <maiaramartins093@gmail.com>
2021-04-29 09:36:21 -0300

    Initial Commit

---
+++ laFiesta/laFiestaApp.swift
@@ -0,0 +1,17 @@
+//
+//  laFiestaApp.swift
+//  laFiesta
+//
+//  Created by Maiara Martins on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct laFiestaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
