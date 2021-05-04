6cb9d94a88c561b811d201dddc7e7bb6de4e934b
Caroline Intaschi <intaschi.caroline@gmail.com>
2021-05-04 11:55:41 -0300

    Card transformado em componente

---
+++ laFiesta/Views/EventCardView.swift
@@ -0,0 +1,20 @@
+//
+//  EventCardView.swift
+//  laFiesta
+//
+//  Created by Caroline Andreoni Barcat Intaschi on 04/05/21.
+//
+
+import SwiftUI
+
+struct EventCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventCardView()
+    }
+}
