7480bc0b45b466296769738c1226e5deef448483
Erick <erickgvieira@hotmail.com>
2021-05-05 11:55:10 -0300

    Navegação entre as telas de explore events - detail - qrcode

--- laFiesta/Views/ExploreEventsView.swift
+++ laFiesta/Views/ExploreEventsView.swift
@@ -5,7 +5,7 @@ import SwiftUI
 struct ExploreEventsView: View {
 
     private var events2:[EventModel]
-    
+
     init() {
         events2 = eventsData
     }
@@ -13,54 +13,61 @@ struct ExploreEventsView: View {
     @State public var search: String = ""
     
     var body: some View {
-        VStack(alignment: .leading){
-            Text("Explore Events")
-                .foregroundColor(.white)
-                .font(.system(size: 34))
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ZStack{
+                Color.black.ignoresSafeArea()
 
-            Text("Start looking for events names and places")
-                .foregroundColor(.white)
-                .font(.system(size: 15))
-                .padding(.bottom)
-
-            ZStack(alignment: .leading) {
-                TextField("Search...", text: $search)
-                    .background(Color(#colorLiteral(red: 0.462745098, green: 0.462745098, blue: 0.5019607843, alpha: 1)))
-                    .font(.system(size: 17))
-                    .foregroundColor(Color(#colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9607843137, alpha: 1)))
-
-                    if search == "" {
-                        HStack{
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(Color(#colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9607843137, alpha: 1)))
-
-                            Text("Search...").foregroundColor(Color(#colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9607843137, alpha: 1)))
-                        }.font(.system(size: 17))
+                VStack{
+                    HStack{
+                        Text("Explore Events")
+                            .foregroundColor(.white)
+                            .font(.system(size: 34))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        
+                        Spacer()
                     }
-            }
+                    HStack{
+                        Text("Start looking for events names and places")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15))
+                            .padding(.bottom)
+                        
+                        Spacer()
+                    }
 
-            if search == "" {
-                List(){
-                    VStack(spacing: 0){
-                        ForEach(events2, id: \.self) { event in
-                            EventCardView(eventForCard: event)
+
+                    ZStack(alignment: .leading) {
+                        TextField("Search...", text: $search)
+                            .background(Color(#colorLiteral(red: 0.462745098, green: 0.462745098, blue: 0.5019607843, alpha: 1)))
+                            .font(.system(size: 17))
+                            .foregroundColor(Color(#colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9607843137, alpha: 1)))
+
+                            if search == "" {
+                                HStack{
+                                    Image(systemName: "magnifyingglass")
+                                        .foregroundColor(Color(#colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9607843137, alpha: 1)))
+
+                                    Text("Search...").foregroundColor(Color(#colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9607843137, alpha: 1)))
+                                }.font(.system(size: 17))
+                            }
+                    }.padding(.bottom)
+
+                    ZStack {
+                        if search == "" {
+                            ScrollView(){
+                                ForEach(events2, id: \.self) { event in
+                                    EventCardView(eventForCard: event)
+                                }
+                            }
+                        }
+                        else{
+
                         }
                     }
-                    .offset(x: -20, y: -6)
-                }
+                }.padding()
+                .navigationBarHidden(true)
             }
-            else{
-//                List{
-//                    VStack(spacing: 0){
-//                        ForEach(events2, id: \.self) { event in
-//                            EventCardView(eventForCard: event2)
-//                        }
-//                    }
-//                    .offset(x: -20, y: -20)
-//                }
-            }
-        }.padding()
+        }
     }
 }
 
