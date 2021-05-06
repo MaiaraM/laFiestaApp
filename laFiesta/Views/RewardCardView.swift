4b6bf74f20889236d3107b35f01d6b5da3566dad
Caroline Intaschi <intaschi.caroline@gmail.com>
2021-05-06 14:01:36 -0300

    Carrossel horizontal na tela de pontos e padding final na tela de detalhes

---
+++ laFiesta/Views/RewardCardView.swift
@@ -0,0 +1,20 @@
+//
+//  RewardCardView.swift
+//  laFiesta
+//
+//  Created by Caroline Andreoni Barcat Intaschi on 06/05/21.
+//
+
+import SwiftUI
+
+struct RewardCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RewardCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        RewardCardView()
+    }
+}
